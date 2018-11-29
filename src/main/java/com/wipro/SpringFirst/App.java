bbe9c5cc764ba08b9a1dc92c83ae3006e34a10d7
FaizanAkhtar85 <faizanakhtar35@gmail.com>
2018-11-29 18:39:24 +0530

    gjgj

--- src/main/java/com/wipro/SpringFirst/App.java
+++ src/main/java/com/wipro/SpringFirst/App.java
@@ -10,7 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App
 {
-	//hello
+	//hello h
 	public static void main( String[] args )
 	{
 		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
